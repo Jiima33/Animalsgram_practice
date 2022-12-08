2b37362bc749d375d0a6c1c5eb1feb3db997076e
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-12-08 09:15:58 +0000

    [fix]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,12 +1,12 @@
 class PostCommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
+    @comment = PostComment.new(post_comment_params)
     @post_image = @comment.post_image
     if @comment.save
       @post_image.create_notification_comment!(current_user, @comment.id)
-      redirect_back(fallback_location: post_image_path, notice: '投稿しました')
+      redirect_to post_image_path(@post_image.id), notice: '投稿しました'
     else
-      redirect_back(fallback_location: post_image_path, notice: '投稿に失敗しました')
+      render post_image_path, notice: '投稿に失敗しました'
     end
   end
   
