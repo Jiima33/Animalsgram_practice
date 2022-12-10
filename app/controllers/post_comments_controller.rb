674c0ba54d59ac44e91d74577d17bd7293fafbc0
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-12-10 05:48:57 +0000

    [add]comment ajax

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,23 @@
 class PostCommentsController < ApplicationController
   def create
+    @post_image = PostImage.find(params[:post_image_id])
     @comment = PostComment.new(post_comment_params)
     @post_image = @comment.post_image
     if @comment.save
       @post_image.create_notification_comment!(current_user, @comment.id)
-      redirect_to post_image_path(@post_image.id), notice: '投稿しました'
+      flash.now[:notice] = "コメントしました。"
+      render :create
     else
-      render post_image_path(@post_image.id), notice: '投稿に失敗しました'
+      render :error
     end
   end
   
   
   def destroy
+    @post_image = PostImage.find(params[:post_image_id])
     PostComment.find(params[:id]).destroy
-    redirect_to post_image_path(params[:post_image_id])
+    flash.now[:notice] = "コメントを削除しました。"
+    render :destroy
   end
   
   private
