683837a1d25de6f46c2a91249717c317d522b891
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-12-26 02:09:53 +0000

    [add]フレンドリーフォワーディング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
-  
-  def after_sign_in_path_for(resource)
-    post_images_path
-  end
+  before_action :store_user_location!, if: :storable_location?
 
-
-  
-  def after_sign_out_path_for(resource)
-    about_path
-  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+  
+  private
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,4 +4,10 @@ class Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to user_path(user), notice: 'guestuserでログインしました。'
   end
+  
+  protected
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
 end
